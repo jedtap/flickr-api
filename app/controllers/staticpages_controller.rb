e4606b3d7198b153262a1355b03d0fd27e10ff93
Jed-Tapiador <doom.slaver2@gmail.com>
2022-05-01 19:40:01 +0800

    add flickr api and figaro

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,6 +1,13 @@
 class StaticpagesController < ApplicationController
+  require 'flickr'
 
   def index
+    flickr = Flickr.new
+    unless params[:user_id].blank?
+      @photos = flickr.photos.search(user_id: params[:user_id])
+    else
+      @photos = flickr.photos.getRecent
+    end
   end
-  
+
 end
